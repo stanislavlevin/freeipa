fd59142d4494820bfd44fdf991ec7b39566ed2b4
Stanislav Levin <slev@altlinux.org>
2018-07-26 11:01:35 +0300

    alt: setup apache modules
    The IPA installation requires a special apache configuration.
    There are several required apache modules and there are several
    conflicting apache modules.

--- ipaserver/install/server/upgrade.py
+++ ipaserver/install/server/upgrade.py
@@ -1875,6 +1875,7 @@ def upgrade_configuration():
     migrate_to_mod_ssl(http)
     update_http_keytab(http)
     http.configure_gssproxy()
+    http.configure_httpd_mods()
     http.start()
 
     uninstall_selfsign(ds, http)
