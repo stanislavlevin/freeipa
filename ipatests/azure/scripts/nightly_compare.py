3dd91057daa674ce6ef9bbafc4258000006ee045
Stanislav Levin <slev@altlinux.org>
2020-02-19 09:46:34 +0300

    alt: ap: add ALTLinux tests definitions

---
+++ ipatests/azure/scripts/nightly_compare.py
@@ -0,0 +1,74 @@
+import yaml
+
+PRCI_NIGHTLY = "ipatests/prci_definitions/nightly_latest.yaml"
+PRCI_GATING = "ipatests/prci_definitions/gating.yaml"
+AZURE_NIGHTLY = "ipatests/azure/azure_definitions/nightly.yml"
+AZURE_GATING = "ipatests/azure/azure_definitions/gating.yml"
+
+prci_tests = []
+azure_tests = []
+
+SKIP_IN_AZURE_LIST = [
+    "test_integration/test_advise.py",  # RHEL/Fedora specific
+    "test_integration/test_authselect.py",  # ALT doesn't have authselect
+    "test_integration/test_idviews",  # RunADTests class(2 IPA + 1 AD classes)
+    "test_integration/test_fips.py",  # requires fake FIPS mode
+    "test_integration/test_http_kdc_proxy.py",  # requires AD(should be fixed)
+    # requires external DNS
+    "test_integration/test_installation.py::TestADTrustInstall",
+    "test_integration/test_installation.py::TestInstallWithoutNamed",
+]
+
+EXTRA_AZURE_LIST = [
+    "test_integration/test_idviews.py::TestRulesWithServicePrincipals",
+    "test_integration/test_idviews.py::TestIDViews",
+    # ALT specific tests
+    "test_integration/test_ntp_options.py::TestNTPMissingOptionsAndNTPs",
+    "test_integration/test_ntp_options.py::TestNTPoptionsCommon",
+]
+
+
+def parse_prci_tests(config, tests):
+    with open(config) as f:
+        prci_yaml = yaml.safe_load(f)
+        for task in prci_yaml["jobs"].values():
+            job = task["job"]
+            if job["class"] == "RunPytest":
+                tests.extend(job["args"]["test_suite"].split())
+
+    tests.sort()
+
+
+def parse_azure_tests(config, tests):
+    with open(config) as f:
+        azure_yaml = yaml.safe_load(f)
+        for vm_jobs in azure_yaml["vms"]:
+            for job in vm_jobs["vm_jobs"]:
+                tests.extend(job["tests"])
+
+    tests.sort()
+
+
+parse_prci_tests(PRCI_GATING, tests=prci_tests)
+parse_prci_tests(PRCI_NIGHTLY, tests=prci_tests)
+
+parse_azure_tests(AZURE_GATING, tests=azure_tests)
+parse_azure_tests(AZURE_NIGHTLY, tests=azure_tests)
+
+missing_in_azure = set(prci_tests) - set(azure_tests + SKIP_IN_AZURE_LIST)
+print("missing in Azure tests:", *sorted(missing_in_azure), sep='\n')
+if missing_in_azure:
+    print(
+        "##vso[task.logissue type=warning]"
+        "Missing nightly tests in Azure Pipelines, compared to PR-CI",
+        missing_in_azure,
+    )
+
+extra_in_azure = set(azure_tests) - set(prci_tests + EXTRA_AZURE_LIST)
+print("extra Azure tests:", *sorted(extra_in_azure), sep='\n')
+if extra_in_azure:
+    print(
+        "##vso[task.logissue type=warning]"
+        "Extra nightly tests in Azure Pipelines, compared to PR-CI",
+        extra_in_azure,
+    )
