25d4478921f216491603da3d05a3b19d2def94b8
Stanislav Levin <slev@altlinux.org>
2020-02-19 09:46:34 +0300

    alt: ap: add ALTLinux tests definitions

---
+++ ipatests/azure/scripts/install-debuginfo-altlinux.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+
+function install_debuginfo() {
+    apt-get update && \
+    apt-get install -y \
+        gdb \
+        systemd-coredump \
+        autofs-debuginfo \
+        ${IPA_TESTS_REPO_PATH}/dist/rpms_debuginfo/*.rpm \
+        389-ds-base-debuginfo \
+        libjemalloc2-debuginfo \
+        libsasl2-plugin-gssapi-debuginfo \
+        slapi-nis-debuginfo \
+        apache2-base-debuginfo \
+        apache2-mod_auth_gssapi-debuginfo \
+        apache2-mod_ssl-debuginfo \
+        apache2-mod_wsgi-py3-debuginfo \
+        apache2-mods-debuginfo \
+        bind-debuginfo \
+        bind-utils-debuginfo \
+        bind-dyndb-ldap-debuginfo \
+        libp11-debuginfo \
+        certmonger-debuginfo \
+        gssproxy-debuginfo \
+        krb5-kdc-debuginfo \
+        krb5-kinit-debuginfo \
+        samba-dc-mitkrb5-debuginfo \
+        sssd-debuginfo \
+        sssd-idp-debuginfo
+}
