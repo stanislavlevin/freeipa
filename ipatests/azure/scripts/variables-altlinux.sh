a215424991dd3e7aea000627a24fa13e06b41321
Stanislav Levin <slev@altlinux.org>
2020-02-19 09:46:34 +0300

    alt: ap: add ALTLinux tests definitions

---
+++ ipatests/azure/scripts/variables-altlinux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eu
+
+# Put the platform-specific definitions here
+
+HTTPD_SYSTEMD_NAME='httpd2.service'
+HTTPD_LOGDIR='/var/log/httpd2'
+HTTPD_ERRORLOG="${HTTPD_LOGDIR}/error_log"
+HTTPD_BASEDIR='/etc/httpd2'
+HTTPD_ALIASDIR="${HTTPD_BASEDIR}/conf"
+BIND_BASEDIR='/var/lib/bind'
+BIND_DATADIR="${BIND_BASEDIR}/data"
+BIND_SYSTEMD_NAME='bind.service'
+BIND_LOGGING_OPTIONS_CONF='/etc/bind/ipa-logging-ext.conf'
+
+function installed_packages() {
+    rpm -qa | sort
+}
