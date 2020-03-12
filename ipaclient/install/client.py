c1e6a8f96c66a5b9be60a5a86ca3b52680c25ac1
Stanislav Levin <slev@altlinux.org>
2020-03-12 16:24:32 +0300

    alt: ipaclient: add ALT-specific PAM tasks
    Provides functionality similar to the authselect tool.

--- ipaclient/install/client.py
+++ ipaclient/install/client.py
@@ -3225,6 +3225,7 @@ def _install(options, tdict):
         tasks.modify_nsswitch_pam_stack(
             sssd=options.sssd,
             mkhomedir=options.mkhomedir,
+            fstore=fstore,
             statestore=statestore,
             sudo=options.conf_sudo,
             subid=options.subid
