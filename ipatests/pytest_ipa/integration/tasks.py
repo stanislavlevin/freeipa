7cbe5ae92346ae52077a6a062422d40b74b9f53c
Stanislav Levin <slev@altlinux.org>
2020-09-22 17:07:01 +0300

    alt: ipatests: enable keyboard-interactive auth for sshd
    keyboard-interactive doesn't work for ALT out of the box
    (pam_sss doesn't support pam_userpass).
    This should be removed on implementation of control policy.
    
    See for details:
    https://bugzilla.altlinux.org/38977
    https://sssd.io/design-pages/pam_conversation_for_otp.html

--- ipatests/pytest_ipa/integration/tasks.py
+++ ipatests/pytest_ipa/integration/tasks.py
@@ -2967,6 +2967,28 @@ def move_date(host, chrony_cmd, date_str):
     host.run_command(['date', '-s', date_str])
 
 
+def allow_sshd_interactive_auth(host):
+    """Modifies sshd PAM stack to allow keyboard-interactive auth
+
+    Hack: this is the common task which should be implemented as
+    the control policy
+
+    See, https://bugzilla.altlinux.org/38977
+    """
+    pam_sshd_path = "/etc/pam.d/sshd"
+    pam_sshd = textwrap.dedent(
+        """\
+        #%PAM-1.0
+        auth\tinclude\tcommon-login
+        account\tinclude\tcommon-login
+        password\tinclude\tcommon-login
+        session\tinclude\tcommon-login
+        """
+    )
+    host.put_file_contents(pam_sshd_path, pam_sshd)
+    host.run_command(["cat", pam_sshd_path])
+
+
 def copy_files(source_host, dest_host, filelist):
     """Helper to copy a file from one host to another
     :param source_host: source host of the file to copy
