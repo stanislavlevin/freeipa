4ead380a7e72b60f99905652ad908d632e72810c
Stanislav Levin <slev@altlinux.org>
2020-09-29 12:14:45 +0300

    ipa-restore: Add ALT-specific tasks for full restoration mode
    `Full` mode implies dropped-in configs without running IPA installer.

--- ipaserver/install/ipa_restore.py
+++ ipaserver/install/ipa_restore.py
@@ -482,6 +482,13 @@ class Restore(admintool.AdminTool):
                     paths.UPDATES_DIR, "49-autobind-services.update"
                 )
                 ld.update([autobind_update])
+
+                # ALT specific
+                http.configure_httpd_mods()
+
+                ipautil.run(['control', 'bind-chroot', 'disabled'])
+                ipautil.run(['control', 'system-auth', 'sss'])
+
                 # have the daemons pick up their restored configs
                 tasks.systemd_daemon_reload()
                 # Restart IPA a final time.
