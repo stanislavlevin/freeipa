a57e311997019d5fb189524a1f6cdbec87a80563
Stanislav Levin <slev@altlinux.org>
2021-08-02 13:32:25 +0300

    pylint: Skip deprecated-method for match_hostname
    match_hostname is deprecated since Python3.7.
    FreeIPA supports Python3.6+.
    
    https://docs.python.org/3/library/ssl.html#ssl.match_hostname

--- ipalib/x509.py
+++ ipalib/x509.py
@@ -397,7 +397,10 @@ class IPACertificate:
             for value in values:
                 match_san.append(('DNS', value))
 
-        ssl.match_hostname(match_cert, DNSName(hostname).ToASCII())
+        # deprecated since Python3.7 (our supported Python is 3.6)
+        ssl.match_hostname(  # pylint: disable=deprecated-method
+            match_cert, DNSName(hostname).ToASCII()
+        )
 
 
 def load_pem_x509_certificate(data):
