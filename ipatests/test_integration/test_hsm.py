acd8ef15539b73ad1999597a954f52c19932cb92
Stanislav Levin <slev@altlinux.org>
2021-10-20 20:24:26 +0300

    ap: ipatests: skip tests required changing of time
    Linux kernel(5.6+) supports time namespaces, but Docker doesn't.
    
    See for details:
    https://github.com/moby/moby/issues/39163.
    
    So, the changing of time in container effectively changes time in host.
    This works fine locally, but azure agent doesn't like time travelers:
    \#[error]We stopped hearing from agent Azure Pipelines 2. Verify the agent machine is running and has a healthy network connection. Anything that terminates an agent process, starves it for CPU, or blocks its network access can cause this error. For more information, see: https://go.microsoft.com/fwlink/?linkid=846610

--- ipatests/test_integration/test_hsm.py
+++ ipatests/test_integration/test_hsm.py
@@ -650,6 +650,11 @@ class TestHSMcertFix(BaseHSMTest):
 
     master_with_dns = False
 
+    @pytest.mark.skip_if_hostcontainer(
+        "master",
+        container="any",
+        reason="Containers don't support time namespaces yet",
+    )
     def test_hsm_renew_expired_cert_on_master(self, expire_cert_critical):
         check_version(self.master)
         expire_cert_critical(self.master)
@@ -675,6 +680,11 @@ class TestHSMcertFixKRA(BaseHSMTest):
     master_with_dns = False
     master_with_kra = True
 
+    @pytest.mark.skip_if_hostcontainer(
+        "master",
+        container="any",
+        reason="Containers don't support time namespaces yet",
+    )
     def test_hsm_renew_expired_cert_with_kra(self, expire_cert_critical):
         check_version(self.master)
         expire_cert_critical(self.master)
@@ -718,6 +728,11 @@ class TestHSMcertFixReplica(BaseHSMTest):
             tasks.uninstall_master(host)
             tasks.move_date(host, 'start', '-3years-1days')
 
+    @pytest.mark.skip_if_hostcontainer(
+        "master",
+        container="any",
+        reason="Containers don't support time namespaces yet",
+    )
     def test_hsm_renew_expired_cert_replica(self, expire_certs):
         check_version(self.master)
         # wait for cert expiry
@@ -1186,6 +1201,11 @@ class TestHSMACMEPrune(IntegrationTest):
         super(TestHSMACMEPrune, cls).uninstall(mh)
         delete_hsm_token([cls.master], cls.token_name)
 
+    @pytest.mark.skip_if_hostcontainer(
+        "master",
+        container="any",
+        reason="Containers don't support time namespaces yet",
+    )
     def test_hsm_prune_cert_manual(self, issue_and_expire_acme_cert):
         """Test to prune expired certificate by manual run"""
         if (tasks.get_pki_version(self.master)
@@ -1220,6 +1240,11 @@ class TestHSMACMEPrune(IntegrationTest):
         )
         assert f'CN={self.clients[0].hostname}' not in result.stdout_text
 
+    @pytest.mark.skip_if_hostcontainer(
+        "master",
+        container="any",
+        reason="Containers don't support time namespaces yet",
+    )
     def test_hsm_prune_cert_cron(self, issue_and_expire_acme_cert):
         """Test to prune expired certificate by cron job"""
         if (tasks.get_pki_version(self.master)
