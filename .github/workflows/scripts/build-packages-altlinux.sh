b8ce0bdc1758b9776e8947822deb5e7abc5abfab
Stanislav Levin <slev@altlinux.org>
2021-11-02 17:50:25 +0300

    test gha
    cons:
    - no re-runs of failures
    - no file browser for artifacts
    
    diff:
    - default user for containers:
      GHA: root
      AP: unprivileged user
    - scheduled runs:
      GHA: only base/default branch
      AP: any branch

---
+++ .github/workflows/scripts/build-packages-altlinux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+function build_packages() {
+    gear-rpm -ba -v \
+        --define "_topdir $GITHUB_WORKSPACE/rpmbuild" \
+        --without 'lint' \
+        --without 'fasttest' \
+        --without 'fastlint'
+    mkdir -p "$GITHUB_WORKSPACE"/dist/{,s}rpms
+    mkdir -p "$GITHUB_WORKSPACE/dist/rpms_debuginfo"
+    cp -v "$GITHUB_WORKSPACE"/rpmbuild/SRPMS/freeipa-*.src.rpm "$GITHUB_WORKSPACE/dist/srpms/"
+    find "$GITHUB_WORKSPACE/rpmbuild/RPMS/" -type f \( -not -name "*-debuginfo-*" \
+        -a -name '*.rpm' \) -exec cp {} "$GITHUB_WORKSPACE/dist/rpms/" \;
+    find "$GITHUB_WORKSPACE/rpmbuild/RPMS/" -type f \( -name "*-debuginfo-*" \
+        -a -name '*.rpm' \) -exec cp {} "$GITHUB_WORKSPACE/dist/rpms_debuginfo/" \;
+}
