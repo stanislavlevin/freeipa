ff636dc166c3c2d14f51a9853bef2b05ac8e3fc2
Stanislav Levin <slev@altlinux.org>
2021-11-02 17:50:25 +0300

    test gha
    cons:
    - no re-runs of failures
    - no file browser for artifacts
    
    diff:
    - default user for containers:
      GHA: root
      AP: unprivileged user
    - scheduled runs:
      GHA: only base/default branch
      AP: any branch

---
+++ .github/workflows/scripts/prepare-tox-altlinux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -eu
+
+function prepare_tox() {
+    apt-get install -y nss-tools python3-module-pip
+    runuser -u "$GHA_USER" -- \
+        python3 -m pip install --user --upgrade pip setuptools pycodestyle
+}
