96d7ec473c33852cb81d9fe29fb7faf4c1b73f12
Stanislav Levin <slev@altlinux.org>
2021-12-01 12:04:34 +0300

    ipatests: os-dependent: replace hardcoded systemd names in test_acme

--- ipatests/test_integration/test_acme.py
+++ ipatests/test_integration/test_acme.py
@@ -129,7 +129,8 @@ def certbot_standalone_cert(host, acme_server, no_of_cert=1):
     """method to issue a certbot's certonly standalone cert"""
     # Get a cert from ACME service using HTTP challenge and Certbot's
     # standalone HTTP server mode
-    host.run_command(['systemctl', 'stop', 'httpd'])
+    httpd_name = host.knownservices["httpd"].systemd_name
+    host.run_command(['systemctl', 'stop', httpd_name])
     for _i in range(0, no_of_cert):
         host.run_command(
             [
@@ -350,7 +351,8 @@ class TestACME(CALessBase):
         # If the thing we are inspecting changes, the test will break.
         # So I prefer a conservative sleep.
         #
-        self.clients[0].run_command(['systemctl', 'restart', 'httpd'])
+        httpd_name = self.clients[0].knownservices["httpd"].systemd_name
+        self.clients[0].run_command(['systemctl', 'restart', httpd_name])
         time.sleep(15)
 
         # We expect mod_md has acquired the certificate by now.
@@ -359,7 +361,7 @@ class TestACME(CALessBase):
         # certificates /without/ the second restart, then both
         # of these sleeps can be replaced by "loop until good".)
         #
-        self.clients[0].run_command(['systemctl', 'reload', 'httpd'])
+        self.clients[0].run_command(['systemctl', 'reload', httpd_name])
         time.sleep(3)
 
         # HTTPS request from server to client (should succeed)
