d047f1fdb630cfa6df6d333a510109fef5d74411
Stanislav Levin <slev@altlinux.org>
2021-12-07 12:56:28 +0300

    ipatests: Collect disk usage

--- ipatests/pytest_ipa/integration/__init__.py
+++ ipatests/pytest_ipa/integration/__init__.py
@@ -155,6 +155,7 @@ def collect_test_logs(node, logs_dict, test_config, suffix=''):
     collect_systemd_journal(name, hosts, logfile_dir)
     collect_top(name, hosts, logfile_dir)
     collect_memory_stats(name, hosts, logfile_dir)
+    collect_disk_usage(name, hosts, logfile_dir)
 
 
 def collect_systemd_journal(name, hosts, logfile_dir=None):
@@ -184,6 +185,34 @@ def collect_systemd_journal(name, hosts, logfile_dir=None):
 
         # Write journal to file
         with open(os.path.join(topdirname, "journal"), 'w') as f:
+            f.write(cmd.stdout_text)
+
+
+def collect_disk_usage(name, hosts, logfile_dir=None):
+    """Collect disk usage from remote hosts
+
+    :param name: Name under which logs are collected, e.g. name of the test
+    :param hosts: List of hosts from which to collect disk usage
+    :param logfile_dir: Directory to log to
+    """
+    if logfile_dir is None:
+        return
+
+    for host in hosts:
+        logger.info("Collecting disk usage from: %s", host.hostname)
+
+        topdirname = os.path.join(logfile_dir, name, host.hostname)
+        if not os.path.exists(topdirname):
+            os.makedirs(topdirname)
+
+        cmd = host.run_command(
+            ["df", "-h"], log_stdout=False, raiseonerr=False
+        )
+        if cmd.returncode:
+            logger.error("An error occurred while collecting disk usage")
+            continue
+
+        with open(os.path.join(topdirname, "disk_usage"), "w") as f:
             f.write(cmd.stdout_text)
 
 
