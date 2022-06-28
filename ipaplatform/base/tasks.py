758fb502cf0e6f8f8a7eac918ee0e46e4f267877
Stanislav Levin <slev@altlinux.org>
2022-06-28 14:53:06 +0300

    ALT: nsswitch.conf: Make sss the primary service for automount
    RHEL-based distros rely on default master map configuration which
    includes option '+auto.master':
    > Additionally, a map may be included from its source as if it were
      itself present in the master map by including a line of the form:
      +[maptype,format:]map[options]
    Thereby, all the services specified in nsswitch.conf are searched for
    master map whilst only a first found master map will be used if
    '+auto.master' is not set.
    
    ALTLinux's default configuration for autofs doesn't contain
    '+auto.master'. This and the incorrect order of nsswitch configuration
    for automount results in IPA's automount mappings are ignored.
    
    With this change `ipa-client-automount` sets sss as the primary
    service for automount on installation.

--- ipaplatform/base/tasks.py
+++ ipaplatform/base/tasks.py
@@ -210,6 +210,31 @@ class BaseTaskNamespace:
 
         raise NotImplementedError()
 
+    def enable_nsswitch_automount(self, statestore):
+        """
+        Sets automount database in nsswitch.conf to 'sss' as a primary service.
+
+        RHEL-based distros rely on default master map configuration which
+        includes option '+auto.master':
+        > Additionally, a map may be included from its source as if it were
+          itself present in the master map by including a line of the form:
+          +[maptype,format:]map[options]
+        Thereby, all the services specified in nsswitch.conf are searched for
+        master map whilst only a first found master map will be used if
+        '+auto.master' is not set.
+
+        ALTLinux's default configuration for autofs doesn't contain
+        '+auto.master'. This and the incorrect order of nsswitch configuration
+        for automount results in IPA's automount mappings are ignored.
+        """
+        raise NotImplementedError()
+
+    def disable_nsswitch_automount(self, statestore):
+        """
+        Restores back automount database in nsswitch.conf
+        """
+        raise NotImplementedError()
+
     def modify_pam_to_use_krb5(self, statestore):
         """
         Configure pam stack to allow kerberos authentication.
