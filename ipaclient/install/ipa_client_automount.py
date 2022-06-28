20476d86a2f179926909ef2cab57d08803d29878
Stanislav Levin <slev@altlinux.org>
2022-06-28 14:53:06 +0300

    alt: nsswitch.conf: make sss the primary service for automount
    RHEL-based distros rely on default master map configuration which
    includes option '+auto.master':
    > Additionally, a map may be included from its source as if it were
      itself present in the master map by including a line of the form:
      +[maptype,format:]map[options]
    Thereby, all the services specified in nsswitch.conf are searched for
    master map whilst only a first found master map will be used if
    '+auto.master' is not set.
    
    ALTLinux's default configuration for autofs doesn't contain
    '+auto.master'. This and the incorrect order of nsswitch configuration
    for automount results in IPA's automount mappings are ignored.
    
    With this change `ipa-client-automount` sets sss as the primary
    service for automount on installation.

--- ipaclient/install/ipa_client_automount.py
+++ ipaclient/install/ipa_client_automount.py
@@ -236,6 +236,7 @@ def uninstall(fstore, statestore):
 
     print("Restoring configuration")
 
+    tasks.disable_nsswitch_automount(statestore)
     for filepath in RESTORE_FILES:
         if fstore.has_file(filepath):
             fstore.restore_file(filepath)
@@ -505,6 +506,7 @@ def configure_automount():
         sys.exit("Installation aborted")
 
     try:
+        tasks.enable_nsswitch_automount(statestore)
         configure_nfs(fstore, statestore, options)
         configure_autofs_sssd(fstore, statestore, autodiscover, options)
         configure_autofs_common(fstore, statestore, options)
