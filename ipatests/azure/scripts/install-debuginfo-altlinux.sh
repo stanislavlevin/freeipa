ef47925d110a2f9782e65e2f13df89db39aab276
Stanislav Levin <slev@altlinux.org>
2022-09-21 12:09:37 +0300

    ALT: gha, ap: Install more required debuginfo packages

--- ipatests/azure/scripts/install-debuginfo-altlinux.sh
+++ ipatests/azure/scripts/install-debuginfo-altlinux.sh
@@ -5,8 +5,12 @@ function install_debuginfo() {
     apt-get install -y \
         gdb \
         systemd-coredump \
+        autofs-debuginfo \
         ${IPA_TESTS_REPO_PATH}/dist/rpms_debuginfo/*.rpm \
         389-ds-base-debuginfo \
+        libjemalloc2-debuginfo \
+        libsasl2-plugin-gssapi-debuginfo \
+        slapi-nis-debuginfo \
         apache2-base-debuginfo \
         apache2-mod_auth_gssapi-debuginfo \
         apache2-mod_ssl-debuginfo \
@@ -15,10 +19,12 @@ function install_debuginfo() {
         bind-debuginfo \
         bind-utils-debuginfo \
         bind-dyndb-ldap-debuginfo \
+        libp11-debuginfo \
         certmonger-debuginfo \
         gssproxy-debuginfo \
         krb5-kdc-debuginfo \
         krb5-kinit-debuginfo \
         samba-dc-mitkrb5-debuginfo \
-        sssd-debuginfo
+        sssd-debuginfo \
+        sssd-idp-debuginfo
 }
