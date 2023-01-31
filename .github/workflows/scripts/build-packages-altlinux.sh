f27af6e19ee18c0e25f6ecdc4d251f7662fcb2d3
Stanislav Levin <slev@altlinux.org>
2023-01-31 21:28:04 +0300

    ap, gha: Drop no longer existent 'fastlint' build rule
    `fastlint` build rule was removed in 4.9.11-alt1.

--- .github/workflows/scripts/build-packages-altlinux.sh
+++ .github/workflows/scripts/build-packages-altlinux.sh
@@ -4,8 +4,7 @@ function build_packages() {
     gear-rpm -ba -v \
         --define "_topdir $GITHUB_WORKSPACE/rpmbuild" \
         --without 'lint' \
-        --without 'fasttest' \
-        --without 'fastlint'
+        --without 'fasttest'
     mkdir -p "$GITHUB_WORKSPACE"/dist/{,s}rpms
     mkdir -p "$GITHUB_WORKSPACE/dist/rpms_debuginfo"
     cp -v "$GITHUB_WORKSPACE"/rpmbuild/SRPMS/freeipa-*.src.rpm "$GITHUB_WORKSPACE/dist/srpms/"
