d6b66a5f1706e3275970552f775203ee184909f5
Stanislav Levin <slev@altlinux.org>
2023-02-06 11:59:26 +0300

    ipatests: Install healthcheck before querying its version
    Order of installation/querying `ipa-healthcheck` in
    `TestIpaHealthCheckWithoutDNS::test_ipa_dns_systemrecords_check`
    test can lead to failures:
    - querying the version of `ipa-healthcheck` fails because of
      the missing corresponding package. There is no explicit
      installation of the package before this test, though
      `ipa-healthcheck` may be pulled indirectly or may be pre-installed.
    
    - expected message depends on `ipa-healtchcheck` version and it's
      calculated before updating the package. The assumption will be
      wrong on updating 0.11 -> 0.12 for example.
    
    Related: https://pagure.io/freeipa/issue/9291

--- ipatests/test_integration/test_ipahealthcheck.py
+++ ipatests/test_integration/test_ipahealthcheck.py
@@ -1687,6 +1687,7 @@ class TestIpaHealthCheckWithoutDNS(IntegrationTest):
         Test checks the result of IPADNSSystemRecordsCheck
         when ipa-server is configured without DNS.
         """
+        tasks.install_packages(self.master, HEALTHCHECK_PKG)
         version = tasks.get_healthcheck_version(self.master)
         if (parse_version(version) < parse_version('0.12')):
             expected_msgs = {
@@ -1703,7 +1704,6 @@ class TestIpaHealthCheckWithoutDNS(IntegrationTest):
                 "Expected URI record missing",
             }
 
-        tasks.install_packages(self.master, HEALTHCHECK_PKG)
         returncode, data = run_healthcheck(
             self.master,
             "ipahealthcheck.ipa.idns",
