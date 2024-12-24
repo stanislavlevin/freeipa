6d9a4f4c590454824d11167bf0ea227db2f8cbdf
Stanislav Levin <slev@altlinux.org>
2024-12-24 20:43:30 +0300

    ipatests: skip test_integration/test_caless.py::TestIPACommands
    See for details:
    https://pagure.io/freeipa/issue/9713

--- ipatests/test_integration/test_caless.py
+++ ipatests/test_integration/test_caless.py
@@ -1290,6 +1290,11 @@ class TestIPACommands(CALessBase):
         with self.host():
             self.master.run_command(['ipa', 'host-del', self.test_hostname])
 
+    @pytest.mark.skip_if_hostplatform(
+        "master",
+        platform="altlinux",
+        reason="https://pagure.io/freeipa/issue/9713",
+    )
     def test_invoke_upgrader(self):
         """Test that ipa-server-upgrade runs without error."""
         self.master.run_command(['ipa-server-upgrade'], raiseonerr=True)
