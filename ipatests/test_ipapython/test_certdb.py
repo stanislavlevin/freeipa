e88c2598d838bad04cf901a726912b39d40186c9
Stanislav Levin <slev@altlinux.org>
2025-10-06 12:48:04 +0300

    tests: make xfailing nss dbm tests conditional
    It's xfailed for nss 3.114+ (there is no fix yet).
    This allows seamless backport for systems with nss < 3.114.

--- ipatests/test_ipapython/test_certdb.py
+++ ipatests/test_ipapython/test_certdb.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import os
+import subprocess
 
 import pytest
 
@@ -11,6 +12,7 @@ from ipapython.certdb import (
 )
 from ipapython import ipautil
 from ipaplatform.osinfo import osinfo
+from ipaplatform.tasks import tasks
 
 CERTNICK = 'testcert'
 CERTSAN = 'testcert.certdb.test'
@@ -22,6 +24,22 @@ if osinfo.id == 'fedora':
         NSS_DEFAULT = 'dbm'
 else:
     NSS_DEFAULT = None
+
+
+def nss_3_114_alt1():
+    """
+    Return True if nss is 3.114-alt1 or newer:
+    https://bugzilla.mozilla.org/show_bug.cgi?id=1982807
+    """
+    res=subprocess.run(
+        ["rpm", "-q", "--qf", "%{VERSION}-%{RELEASE}", "libnss"],
+        check=True,
+        capture_output=True,
+        encoding="utf-8",
+    )
+    installed_version = tasks.parse_ipa_version(res.stdout)
+    target_version = tasks.parse_ipa_version("3.114-alt1")
+    return installed_version >= target_version
 
 
 def create_selfsigned(nssdb):
@@ -106,6 +124,7 @@ def test_sql_tmp():
     reason="NSS is built without support of the legacy database(DBM)",
 )
 @pytest.mark.xfail(
+    nss_3_114_alt1(),
     reason=(
         "fails with nss 3.114 "
         "(https://bugzilla.mozilla.org/show_bug.cgi?id=1982807)"
@@ -152,6 +171,7 @@ def test_convert_db():
     reason="NSS is built without support of the legacy database(DBM)",
 )
 @pytest.mark.xfail(
+    nss_3_114_alt1(),
     reason=(
         "fails with nss 3.114 "
         "(https://bugzilla.mozilla.org/show_bug.cgi?id=1982807)"
