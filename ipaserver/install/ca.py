2c937b70a6bd2c5e96f1d3c5a8e4bc45d65e2daa
Stanislav Levin <slev@altlinux.org>
2026-07-30 17:17:34 +0300

    installer: check all the required PKI ports
    Internally (localhost only) PKI uses ports:
    - 8005: the management port of the Tomcat instance
    - 8009: the AJP port of the Tomcat instance
    - 8090: the HTTP port of the Tomcat instance
    - 8443: the HTTPS port of the Tomcat instance
    
    But only the last two of them are actually checked on IPA installation.
    
    In case some other process (e.g. another instance of tomcat) already
    listens on those ports, the IPA installer crashes without a helpful diagnostic:
    ```
    DEBUG The ipa-server-install command failed, exception: RuntimeError: CA configuration failed.
    ERROR CA configuration failed.
    ERROR The ipa-server-install command failed. See /var/log/ipaserver-install.log for more information
    ```
    
    PKI installation logs:
    ```
    server[3407]: SEVERE: Failed to create server shutdown socket on address [localhost] and port [8005]
    server[3407]: java.net.BindException: Address already in use
    ```
    
    Gracefully fail installation with a helpful hint instead.
    
    Similar issue:
    https://pagure.io/freeipa/issue/7415

--- ipaserver/install/ca.py
+++ ipaserver/install/ca.py
@@ -508,8 +508,8 @@ def install_check(standalone, replica_config, options):
     if not options.external_cert_files:
         if not cainstance.check_ports():
             print(
-                "IPA requires ports 8090 and 8443 for PKI, but one or more "
-                "are currently in use."
+                "IPA requires ports 8005, 8009, 8090 and 8443 for PKI, but one "
+                "or more are currently in use."
             )
             raise ScriptError("Aborting installation")
 
